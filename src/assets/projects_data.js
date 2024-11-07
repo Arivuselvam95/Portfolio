c652d5dc65561bd57b42f5f3d7f4ddff51e24674
Arivuselvam95 <arivuselvam439@gmail.com>
2024-11-07 13:32:48 +0530

    reworked projects

--- src/assets/projects_data.js
+++ src/assets/projects_data.js
@@ -2,17 +2,20 @@ const Project_Data = [
     {
         p_no:"01",
         p_name:"Ecommerce-platform",
-        p_desc:"Platform that promotes sales of organic veggies, products..."
+        p_desc:"Platform that promotes sales of organic veggies, products...",
+        p_link:"https://github.com/Arivuselvam95/Ecommerce-Web--Django.git",
     },
     {
         p_no:"02",
         p_name:"Agri-Price Prediction",
-        p_desc:"Web that helps farmers to predict their crops prices..."
+        p_desc:"Web that helps farmers to predict their crops prices...",
+        p_link:"https://github.com/Arivuselvam95/Agri-PricePrediction.git",
     },
     {
         p_no:"03",
         p_name:"Gemini-clone",
-        p_desc:"Web development is the process of building, programming..."
+        p_desc:"Web development is the process of building, programming...",
+        p_link:"https://github.com/Arivuselvam95/Gemini.git",
     }
    
 ]
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,7 +3,10 @@ import './Projects.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
 import Project_Data from '../../assets/projects_data'
 import arrow_icon from '../../assets/arrow_icon.svg'
+// import { useNavigate } from 'react-router-dom'
+
 const Projects = () => {
+  // const navigate=useNavigate();
   return (
     <div id='projects' className='projects'>
       <div className="projects-title">
@@ -12,10 +15,11 @@ const Projects = () => {
       </div>
       <div className="projects-container">
         {Project_Data.map((project,index)=>{
-            return <div key={index} className='projects-format'>
+            return <div key={index} className='projects-format' onClick={()=> {window.location.href =project.p_link}}>
                         <h3>{project.p_no}</h3>
                         <h2>{project.p_name}</h2>
                         <p>{project.p_desc}</p>
+                        
                         <div className='project-readmore'>
                             <p>Read More</p>
                             <img src={arrow_icon} alt="" />
